796b4be7f837d25bc57200a076510e8a81dd0e27
hyun.yi <hyun.yi@xing.com>
2018-05-26 17:03:32 +0100

    register app to page and make bot answer

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 'use strict';
 
-// Imports dependencies and set up http server
 const
   express = require('express'),
   bodyParser = require('body-parser'),
   request = require('request'),
   app = express().use(bodyParser.json()); // creates express http server
-
-// Sets server port and logs message on success
 const myPort = process.env.PORT || 5000;
+
 app.listen(myPort, () => console.log('webhook is listening', myPort));
 
 app.get('/', (req, res) => {
   res.send("Yes, so this is the root page...");
 });
 
-// Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
  
   let body = req.body;
@@ -30,6 +27,18 @@ app.post('/webhook', (req, res) => {
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);        
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
     });
 
     res.status(200).send('EVENT_RECEIVED');
@@ -39,10 +48,9 @@ app.post('/webhook', (req, res) => {
 
 });
 
-// Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
   // Your verify token. Should be a random string.
-  let verifyToken = process.env.VERIFY_TOKEN;
+  const verifyToken = process.env.VERIFY_TOKEN;
 
   // Parse the query params
   let mode = req.query['hub.mode'];
@@ -62,3 +70,89 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
+
+function handleMessage(sender_psid, received_message) {
+
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);    
+}
+
+function callSendAPI(sender_psid, response) {
+  const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;  
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+}
+
+function handleMessage(sender_psid, received_message) {
+  let response;
+  
+  // Checks if the message contains text
+  if (received_message.text) {    
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Is this the right picture?",
+            "subtitle": "Tap a button to answer.",
+            "image_url": attachment_url,
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Yes!",
+                "payload": "yes",
+              },
+              {
+                "type": "postback",
+                "title": "No!",
+                "payload": "no",
+              }
+            ],
+          }]
+        }
+      }
+    }
+  } 
+  
+  // Send the response message
+  callSendAPI(sender_psid, response);    
+}
