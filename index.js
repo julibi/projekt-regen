b25baafced07e4309e8ebaddc6a4325820f1640b
hyun.yi <hyun.yi@xing.com>
2018-05-26 11:46:16 +0100

    setup local envs

--- index.js
+++ index.js
@@ -7,39 +7,37 @@ const
   app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 5000, () => console.log('webhook is listening'));
+const myPort = process.env.PORT || 5000;
+app.listen(myPort, () => console.log('webhook is listening', myPort));
 
 // Creates the endpoint for our webhook 
-// app.post('/webhook', (req, res) => {  
+app.post('/webhook', (req, res) => {  
  
-//   let body = req.body;
+  let body = req.body;
 
-//   // Checks this is an event from a page subscription
-//   if (body.object === 'page') {
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
 
-//     // Iterates over each entry - there may be multiple if batched
-//     body.entry.forEach(function(entry) {
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
 
-//       // Gets the message. entry.messaging is an array, but 
-//       // will only ever contain one message, so we get index 0
-//       let webhook_event = entry.messaging[0];
-//       console.log(webhook_event);
-//     });
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
 
-//     // Returns a '200 OK' response to all requests
-//     res.status(200).send('EVENT_RECEIVED');
-//   } else {
-//     // Returns a '404 Not Found' if event is not from a page subscription
-//     res.sendStatus(404);
-//   }
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    res.sendStatus(404);
+  }
 
-// });
+});
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
-
   // Your verify token. Should be a random string.
-  let verifyToken = process.env.ACCESS_TOKEN;
+  let verifyToken = 'busted';
     
   // Parse the query params
   let mode = req.query['hub.mode'];
@@ -51,13 +49,10 @@ app.get('/webhook', (req, res) => {
   
     // Checks the mode and token sent is correct
     if (mode === 'subscribe' && token === verifyToken) {
-      
-      // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
     
     } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);      
     }
   }
