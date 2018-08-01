ca50038e16d55f4d855925a6441f01d752994e06
hyun.yi <hyun.yi@xing.com>
2018-08-01 11:20:39 +0200

    remove double function

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function handleMessage(sender_psid, received_message) {
   let response;
 
   fetch('https://vast-gorge-26239.herokuapp.com/')
-	  .then(res => console.log(res.json(), 'hello i just called polar-castle'));
+	  .then(res => console.log(res.json(), 'hello i just called vast-gorge'));
 
   // Check if the message contains text
   if (received_message.text === 'starte Projekt regen') {    
@@ -121,46 +121,46 @@ function callSendAPI(sender_psid, response) {
   }); 
 }
 
-function handleMessage(sender_psid, received_message) {
-  let response;
+// function handleMessage(sender_psid, received_message) {
+//   let response;
   
-  // Checks if the message contains text
-  if (received_message.text) {    
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-    }
-  } else if (received_message.attachments) {
-    // Get the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
-    response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": "Is this the right picture?",
-            "subtitle": "Tap a button to answer.",
-            "image_url": attachment_url,
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Yes!",
-                "payload": "yes",
-              },
-              {
-                "type": "postback",
-                "title": "No!",
-                "payload": "no",
-              }
-            ],
-          }]
-        }
-      }
-    }
-  } 
+//   // Checks if the message contains text
+//   if (received_message.text) {    
+//     // Create the payload for a basic text message, which
+//     // will be added to the body of our request to the Send API
+//     response = {
+//       "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+//     }
+//   } else if (received_message.attachments) {
+//     // Get the URL of the message attachment
+//     let attachment_url = received_message.attachments[0].payload.url;
+//     response = {
+//       "attachment": {
+//         "type": "template",
+//         "payload": {
+//           "template_type": "generic",
+//           "elements": [{
+//             "title": "Is this the right picture?",
+//             "subtitle": "Tap a button to answer.",
+//             "image_url": attachment_url,
+//             "buttons": [
+//               {
+//                 "type": "postback",
+//                 "title": "Yes!",
+//                 "payload": "yes",
+//               },
+//               {
+//                 "type": "postback",
+//                 "title": "No!",
+//                 "payload": "no",
+//               }
+//             ],
+//           }]
+//         }
+//       }
+//     }
+//   } 
   
-  // Send the response message
-  callSendAPI(sender_psid, response);    
-}
+//   // Send the response message
+//   callSendAPI(sender_psid, response);    
+// }
