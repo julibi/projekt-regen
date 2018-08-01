d9f3e5b14e29304ef36bc763626c0fcec8616de2
hyun.yi <hyun.yi@xing.com>
2018-08-01 11:35:59 +0200

    changing handleMessage

--- index.js
+++ index.js
@@ -75,28 +75,28 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-function handleMessage(sender_psid, received_message) {
+// function handleMessage(sender_psid, received_message) {
 
-  let response;
+//   let response;
 
-  fetch('https://vast-gorge-26239.herokuapp.com/')
-	  .then(res => console.log(res.json(), 'hello i just called vast-gorge'));
+//   fetch('https://vast-gorge-26239.herokuapp.com/')
+// 	  .then(res => console.log(res.json(), 'hello i just called vast-gorge'));
 
-  // Check if the message contains text
-  if (received_message.text === 'starte Projekt regen') {    
-    // Create the payload for a basic text message
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`
-    }
-  }  else {
-    response = {
-      "text": `You sent the message: "${received_message.text}"`
-    }
-  }
+//   // Check if the message contains text
+//   if (received_message.text === 'starte Projekt regen') {    
+//     // Create the payload for a basic text message
+//     response = {
+//       "text": `You sent the message: "${received_message.text}". Now send me an image!`
+//     }
+//   }  else {
+//     response = {
+//       "text": `You sent the message: "${received_message.text}"`
+//     }
+//   }
   
-  // Sends the response message
-  callSendAPI(sender_psid, response);    
-}
+//   // Sends the response message
+//   callSendAPI(sender_psid, response);    
+// }
 
 function callSendAPI(sender_psid, response) {
   const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;  
@@ -123,46 +123,46 @@ function callSendAPI(sender_psid, response) {
   }); 
 }
 
-// function handleMessage(sender_psid, received_message) {
-//   let response;
+function handleMessage(sender_psid, received_message) {
+  let response;
   
-//   // Checks if the message contains text
-//   if (received_message.text) {    
-//     // Create the payload for a basic text message, which
-//     // will be added to the body of our request to the Send API
-//     response = {
-//       "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-//     }
-//   } else if (received_message.attachments) {
-//     // Get the URL of the message attachment
-//     let attachment_url = received_message.attachments[0].payload.url;
-//     response = {
-//       "attachment": {
-//         "type": "template",
-//         "payload": {
-//           "template_type": "generic",
-//           "elements": [{
-//             "title": "Is this the right picture?",
-//             "subtitle": "Tap a button to answer.",
-//             "image_url": attachment_url,
-//             "buttons": [
-//               {
-//                 "type": "postback",
-//                 "title": "Yes!",
-//                 "payload": "yes",
-//               },
-//               {
-//                 "type": "postback",
-//                 "title": "No!",
-//                 "payload": "no",
-//               }
-//             ],
-//           }]
-//         }
-//       }
-//     }
-//   } 
+  // Checks if the message contains text
+  if (received_message.text) {    
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Is this the right picture?",
+            "subtitle": "Tap a button to answer.",
+            "image_url": attachment_url,
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Yes!",
+                "payload": "yes",
+              },
+              {
+                "type": "postback",
+                "title": "No!",
+                "payload": "no",
+              }
+            ],
+          }]
+        }
+      }
+    }
+  } 
   
-//   // Send the response message
-//   callSendAPI(sender_psid, response);    
-// }
+  // Send the response message
+  callSendAPI(sender_psid, response);    
+}
